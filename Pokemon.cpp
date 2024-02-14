6e598f8ee56090369be3652346329989fa7889e3
Joseph Sun <jossun@umich.edu>
2024-02-14 16:44:27 -0500

    asd

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -95,14 +95,30 @@ std::istream & operator>>(std::istream &is, Pokemon &pokemon) {
 //          - Otherwise, the pokemon with the highest level wins (in
 //            case of a tie, pokemon a wins).
 bool Pokemon_battle(const Pokemon &a, const Pokemon &b) {
-
-
-
-
-
-  
-  return false; // TODO: replace with your implementation
-
+  //case 1
+  if (a.get_level() > (b.get_level() + 10)) {
+    return true;
+  }
+  else if (b.get_level() > (a.get_level() + 10)) {
+    return false;
+  }
+  //case 2
+  if (a.is_effective_against(b.get_type())) {
+    return true;
+  }
+  else if (b.is_effective_against(a.get_type())) {
+    return false;
+  }
+  //case 3
+  if (a.get_level() > b.get_level()) {
+    return true;
+  }
+  else if (b.get_level() > a.get_level()) {
+    return false;
+  }
+  else {
+    return true;
+  }
 
 
 
